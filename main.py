a38777785a9ef75918d3d0904b5e3f7377829eac
fbettencourt22 <fsbettencourt22@gmail.com>
2025-07-22 12:15:13 +0100

    nova func

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import datetime
 nome: str = input("Qual é o seu nome? ")
 i: str = input("Qual é a tua data de nascimento (no formato DD/MM/AAAA)? ")
 
-data_nascimento = datetime.datetime.strptime(i, "%d/%m/%Y")
+def calcular_idade():
+    data_nascimento = datetime.datetime.strptime(i, "%d/%m/%Y")
 
-data_hoje = datetime.datetime.now()
+    data_hoje = datetime.datetime.now()
 
-idade = data_hoje.year - data_nascimento.year
+    idade = data_hoje.year - data_nascimento.year
 
-if (data_hoje.month, data_hoje.day) < (data_nascimento.month, data_nascimento.day):
-    idade -= 1
+    if (data_hoje.month, data_hoje.day) < (data_nascimento.month, data_nascimento.day):
+        idade -= 1
+        
+    return idade
 
-def welcome():           
+def welcome(nome, idade):  
+            
     if idade > 130:
         print("Erro: idade demasiado alta. Verifique a data de nascimento.")
     else:    
@@ -20,11 +24,14 @@ def welcome():
 
 
 
-def entrar():
-    welcome()
+def entrar(idade):
+    if not welcome(nome, idade):
+        return
+
     if idade >= 18:
         print(f"Como tens {idade}, podes entrar na discoteca. ")
     else:
         print(f"Como tens {idade}, não podes entrar na discoteca. ")
 
-entrar()
+idade = calcular_idade()
+entrar(idade)
