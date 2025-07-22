d5e24767c02aa9035c2f4f2c7efa9ead5cffd722
fbettencourt22 <fsbettencourt22@gmail.com>
2025-07-22 11:41:37 +0100

    adicionar idade

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 print("Olá turma do Python!")
 print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
-print(f"Olá, {nome}!")
+idade: int = input("Qual é a sua idade? ")
+print(f"Olá, {nome}, a tua idade é {idade}!")
+
+
