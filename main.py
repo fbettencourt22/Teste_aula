bc4ee534ab8a1638ad7cf7cf09c4854e7672633f
fbettencourt22 <fsbettencourt22@gmail.com>
2025-07-22 11:54:59 +0100

    verificacao de idade

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ i: int = input("Qual é a tua data de nascimento (no formato DD/MM/AAAA) ?  ")
 data_nascimento = datetime.datetime.strptime(i, "%d/%m/%Y")
 data_hoje = datetime.datetime.now()
 idade = data_hoje.year - data_nascimento.year
-print(f"Olá, {nome}, a tua idade é {idade}!")
+            
+if idade > 130:
+    print("Erro: idade demasiado alta. Verifique a data de nascimento.")
+else:    
+    print(f"Olá, {nome}, a tua idade é {idade}!")
 
 
