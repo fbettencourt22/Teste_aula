1614be5b426a4d69d5febfce00907274ff52403d
fbettencourt22 <fsbettencourt22@gmail.com>
2025-07-22 11:48:54 +0100

    wip

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-print("Olá turma do Python!")
-print("Tudo bem!")
+import datetime
+
 nome: str = input("Qual é o seu nome? ")
-idade: int = input("Qual é a sua idade? ")
+i: int = input("Qual é a tua data de nascimento (no formato DD/MM/AAAA) ?  ")
+data_nascimento = datetime.datetime.strptime(i, "%d/%m/%Y")
+data_hoje = datetime.datetime.now()
+idade = data_hoje.year - data_nascimento.year
 print(f"Olá, {nome}, a tua idade é {idade}!")
 
 
